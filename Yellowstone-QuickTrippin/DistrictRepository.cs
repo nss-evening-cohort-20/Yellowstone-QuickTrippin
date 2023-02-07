b540489a310efa36e5746db0e22ae2fba8ffa108
Danny Herrmann <dannyherrmann91@gmail.com>
2023-02-06 20:45:01 -0600

    creating class files

---
+++ Yellowstone-QuickTrippin/DistrictRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yellowstone_QuickTrippin;
+
+public class DistrictRepository
+{
+    // we're using a static list here instead of a proper database
+    // but the general idea still holds up once we cover databases
+    // it's just another piece of code to replace and refactor nbd
+    static List<District> _districts = new List<District>();
+
+    public List<District> GetDistricts()
+    {
+        return _districts;
+    }
+
+    public void SaveNewDistrict(District district)
+    {
+        _districts.Add(district);
+    }
+}
+
+class StoreRepository
+{
+
+    static List<Store> _stores = new List<Store>();
+
+    public List<Store> GetStores()
+
+    {
+        return _stores;
+    }
+}
