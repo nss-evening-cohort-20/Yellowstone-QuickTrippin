32868b7c33cfcca41c92545524e0a61be57eb3a7
Thomas Kibby <htkibby@gmail.com>
2023-02-07 18:46:08 -0600

    Added values to enums

--- Yellowstone-QuickTrippin/MenuOption.cs
+++ Yellowstone-QuickTrippin/MenuOption.cs
@@ -1,11 +1,17 @@
-﻿namespace Yellowstone_QuickTrippin
+﻿using System.Runtime.Serialization;
+
+namespace Yellowstone_QuickTrippin
 {
     public enum MenuOption
     {
         None = 0,
+        [EnumMember(Value ="Enter District Sales")]
         EnterDistrictSales = 1,
+        [EnumMember(Value = "Generate District Report")]
         GenerateDistricReport = 2,
+        [EnumMember(Value = "Add New Employee")]
         AddNewEmployee = 3,
+        [EnumMember(Value = "Add Store/District")]
         AddStoreOrDistrict = 4,
         Exit = 5,
     }
