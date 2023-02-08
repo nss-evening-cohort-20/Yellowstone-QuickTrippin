85ed95fb4395582f51f60d716d75f256b5987e8b
Danny Herrmann <dannyherrmann91@gmail.com>
2023-02-07 18:57:05 -0600

    working on enum display

---
+++ Yellowstone-QuickTrippin/QTApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yellowstone_QuickTrippin;
+
+public class QTApp
+{
+    public void Run()
+    {
+        Console.WriteLine(OptionsText());
+        Console.ReadLine();
+    }
+
+    private string OptionsText()
+    {
+        StringBuilder builder = new StringBuilder();
+
+        foreach (var option in Enum.GetValues<MenuOption>())
+        {
+            builder.AppendLine($" {(int)option}. {Enum.GetName(option)}");
+        }
+
+        return builder.ToString();
+    }
+}
