2a9e2aeff0d165a1ed424856594b8f4558d93fe4
Stephen Rudge <sbrudge@comcast.net>
2023-02-07 20:24:04 -0600

    updated employee

--- Yellowstone-QuickTrippin/Employee.cs
+++ Yellowstone-QuickTrippin/Employee.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,24 @@ public class Employee
 {
     public string Name { get; set; }
     public string Role { get; set; }
-    public decimal Sales { get; set; }
+    public decimal Sales { get; set; } // arbitrary number
     public int Store { get; set; }
+
+    public Employee(string name, string role, decimal sales)
+    {
+        Name = name;
+        Role = role;
+        Sales = sales;
+    }
+    public void PrintEmployeeInfo()
+    {
+        Console.WriteLine($@"
+{Name}
+{Role}
+Retail Sales: {Sales}
+");
+    }
+
 }
+//print employee info 
+
